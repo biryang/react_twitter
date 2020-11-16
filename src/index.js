61a3c7bc80a798afcc2df6e304f6b2cf713124cb
biryang <hwang7531@gmail>
2020-11-16 19:54:47 +0900

    firebase init

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import firebase from "./firebase/firebase"
+import reportWebVitals from './reportWebVitals'
 
+console.log(firebase)
 ReactDOM.render(
   <React.StrictMode>
     <App />
