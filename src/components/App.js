ea4d93482c51a5d618496272215fd40708c717b5
biryang <56025411+biryang@users.noreply.github.com>
2020-11-25 23:20:55 +0900

    add getting firestore

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,16 @@ import AppRouter from 'components/Router';
 import { authService } from 'firebase/AppFirebase'
 
 function App() {
-  console.log(authService.currentUser)
+  // console.log(authService.currentUser)
   const [init, setInit] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   useEffect(() => {
     //사용자를 가져올 때 Auth 객체가 중간 단계(초기화 등)에 있지 않도록 할 수 있다.
     //로그인 여부 판별
     authService.onAuthStateChanged((user) => {
-      if(user) {
+      if (user) {
         setIsLoggedIn(true);
-      }else{
+      } else {
         setIsLoggedIn(false)
       }
       setInit(true)
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { dbService } from 'firebase/AppFirebase';
 
 const Home = props => {
   const [tweet, setTweet] = useState("")
+  const [tweets, setTweets] = useState([])
+  const getTweets = async () => {
+    const dbTweets = await dbService.collection("tweets").get()
+    dbTweets.forEach((document) => {
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      }
+      setTweets((prev) => [tweetObject, ...prev])
+    })
+  }
+  useEffect(() => {
+    getTweets()
+  }, [])
   const onSubmit = async (event) => {
     event.preventDefault()
     await dbService.collection("tweets").add({
@@ -20,12 +34,19 @@ const Home = props => {
     setTweet(value)
     console.log(tweet)
   }
+  console.log(tweets)
   return (
     <div>
       <form onSubmit={onSubmit} >
         <input type="text" value={tweet} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="Tweet" />
       </form>
+      <div>
+        {tweets.map(tweet =>
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>)}
+      </div>
     </div>
   );
 };
